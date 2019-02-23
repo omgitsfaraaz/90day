a5f036973ac5f17392c4f8c98c5c2958e74cde79
Mohsin <mohsinsait23@gmail.com>
2019-02-23 20:20:44 +0530

    Naive algorithm for Pattern Searching

---
+++ day1/naive.py
@@ -0,0 +1,22 @@
+ 
+def search(pat, txt): 
+	M = len(pat) 
+	N = len(txt) 
+
+	
+	for i in range(N - M + 1): 
+		j = 0
+		
+ 
+		for j in range(0, M): 
+			if (txt[i + j] != pat[j]): 
+				break
+
+		if (j == M - 1): 
+			print("Pattern found at index ", i) 
+
+ 
+if __name__ == '__main__': 
+	txt = "AABAACAADAABAAABAA"
+	pat = "AABA"
+	search(pat, txt)  
