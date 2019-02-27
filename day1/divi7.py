0d7e47ac14cad67f93c7ef7b33203a7cbc263b99
Mohsin <mohsinsait23@gmail.com>
2019-02-27 23:47:53 +0530

    Check divisibility by 7

---
+++ day1/divi7.py
@@ -0,0 +1,20 @@
+
+def isDivisibleBy7(num) : 
+	
+	if num < 0 : 
+		return isDivisibleBy7( -num ) 
+
+	if( num == 0 or num == 7 ) : 
+		return True
+	
+	if( num < 10 ) : 
+		return False
+		
+	return isDivisibleBy7( num / 10 - 2 * ( num - num / 10 * 10 ) ) 
+	
+num = 616
+if(isDivisibleBy7(num)) : 
+	print("Divisible")
+else : 
+	print("Not Divisible")
+
