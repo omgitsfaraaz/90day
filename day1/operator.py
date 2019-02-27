f1829c1dabe1f0d75bbb5e19e6b23389dc384f23
Mohsin <mohsinsait23@gmail.com>
2019-02-27 23:30:28 +0530

    Arithmetic operators

---
+++ day1/operator.py
@@ -0,0 +1,16 @@
+a = 9
+b = 4
+
+add = a + b 
+sub = a - b 
+mul = a * b 
+div1 = a / b 
+div2 = a // b 
+mod = a % b 
+
+print(add) 
+print(sub) 
+print(mul) 
+print(div1) 
+print(div2) 
+print(mod) 
