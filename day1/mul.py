0d6ffa909bc7f2b6f0d1caa98ba1ed70c53b6f45
Mohsin <mohsinsait23@gmail.com>
2019-03-04 22:35:54 +0530

    Taking multiple inputs

---
+++ day1/mul.py
@@ -0,0 +1,18 @@
+
+x, y = input("Enter a two value: ").split() 
+print("Number of boys: ", x) 
+print("Number of girls: ", y) 
+print() 
+
+x, y, z = input("Enter a three value: ").split() 
+print("Total number of students: ", x) 
+print("Number of boys is : ", y) 
+print("Number of girls is : ", z) 
+print() 
+
+a, b = input("Enter a two value: ").split() 
+print("First number is {} and second number is {}".format(a, b)) 
+print() 
+
+x = list(map(int, input("Enter a multiple value: ").split())) 
+print("List of students: ", x) 
