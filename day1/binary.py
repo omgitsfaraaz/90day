290d99051f5222470f4a5704196d3adfe50d7dbe
Mohsin <mohsinsait23@gmail.com>
2019-03-05 22:22:21 +0530

    some changes

--- day1/binary.py
+++ day1/binary.py
@@ -31,4 +31,4 @@ result = binarySearch(arr, 0, len(arr)-1, x)
 if result != -1: 
 	print("Element is present at index %d" % result) 
 else: 
-	print("Element is not present in array")
+	print("Element is not present in array"
