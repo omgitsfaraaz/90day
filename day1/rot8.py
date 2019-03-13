97bff8ee8960f764e101e35d658bdbfd6b26189d
Mohsin <mohsinsait23@gmail.com>
2019-03-13 22:50:37 +0530

    Count rotations divisible by 8

---
+++ day1/rot8.py
@@ -0,0 +1,51 @@
+
+def countRotationsDivBy8(n): 
+	l = len(n) 
+	count = 0
+
+	if (l == 1): 
+		oneDigit = int(n[0]) 
+		if (oneDigit % 8 == 0): 
+			return 1
+		return 0
+
+	if (l == 2): 
+
+		first = int(n[0]) * 10 + int(n[1]) 
+
+		second = int(n[1]) * 10 + int(n[0]) 
+
+		if (first % 8 == 0): 
+			count+=1
+		if (second % 8 == 0): 
+			count+=1
+		return count 
+
+	threeDigit=0
+	for i in range(0,(l - 2)): 
+		threeDigit = (int(n[i]) * 100 +
+					int(n[i + 1]) * 10 +
+					int(n[i + 2])) 
+		if (threeDigit % 8 == 0): 
+			count+=1
+
+	threeDigit = (int(n[l - 1]) * 100 +
+				int(n[0]) * 10 +
+				int(n[1])) 
+
+	if (threeDigit % 8 == 0): 
+		count+=1
+
+	threeDigit = (int(n[l - 2]) * 100 +
+				int(n[l - 1]) * 10 +
+				int(n[0])) 
+	if (threeDigit % 8 == 0): 
+		count+=1
+
+	return count 
+
+
+if __name__=='__main__': 
+	n = "43262488612"
+	print("Rotations:",countRotationsDivBy8(n)) 
+
